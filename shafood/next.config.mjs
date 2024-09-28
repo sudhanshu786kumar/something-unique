27d1cc86b8d45ac6714fbfb28f067ce2a39d2021
sudhanshu kumar <sudhanshu786kumar@gmail.com>
2024-09-28 19:51:44 +0530

    ff

--- shafood/next.config.mjs
+++ shafood/next.config.mjs
@@ -1,7 +1,6 @@
 // next.config.js
 module.exports = {
-    experimental: {
-      staticPageGenerationTimeout: 180, // Increase the timeout to 120 seconds
-    },
-  };
-  
+    // time in seconds of no pages generating during static
+    // generation before timing out
+    staticPageGenerationTimeout: 1000,
+  }
