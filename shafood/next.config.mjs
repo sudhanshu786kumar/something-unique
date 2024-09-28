47324ffd5e575fba1056cd5fe8e968d6798be845
sudhanshu kumar <sudhanshu786kumar@gmail.com>
2024-09-28 19:53:58 +0530

    rtr

--- shafood/next.config.mjs
+++ shafood/next.config.mjs
@@ -1,6 +1,6 @@
-// next.config.js
-module.exports = {
-    // time in seconds of no pages generating during static
-    // generation before timing out
-    staticPageGenerationTimeout: 1000,
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    staticPageGenerationTimeout: 180, // Increase th
+};
+
+export default nextConfig;
