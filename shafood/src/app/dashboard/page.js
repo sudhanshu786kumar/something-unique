4202fcd0ccf3dedafdc5b1a1def40323d832ac22
sudhanshu <sudhanshu786kumar@gmail.com>
2025-05-25 12:46:38 +0530

    Refactor geolocation handling in Page component and clean up unused code

--- shafood/src/app/dashboard/page.js
+++ shafood/src/app/dashboard/page.js
@@ -1,30 +1,18 @@
-"use client"
-import { useEffect, useState } from 'react'
+
 import { Providers } from '../providers'
 import Dashboard from './Dashboard'
 import EnableLocation from '../components/EnableLocation';
-
+import useGeolocation from '../hooks/useGeolocation';
 export default function Page() {
-  const [locationEnabled, setLocationEnabled] = useState(false);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        () => setLocationEnabled(true),
-        () => setLocationEnabled(false)
-      );
-    } else {
-      setLocationEnabled(false);
-    }
-  }, []);
-
+  const location = useGeolocation;
+ console.log(location)
   return (
     <Providers>
-      {locationEnabled ? (
+    
         <Dashboard />
-      ) : (
+      {/* ) : (
        <EnableLocation/>
-      )}
+      )} */}
     </Providers>
   );
 }
